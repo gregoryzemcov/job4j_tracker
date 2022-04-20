9aad3799930d1da212b01fca394dd507fe41d42c
gregoryzemcov <griffondark@mail.ru>
2022-04-20 12:23:08 +0300

    1.2. Вызов метода объекта [#166085]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,9 +1,18 @@
 package ru.job4j.oop;
 
 public class Cat {
+    public void voice2() {
+        System.out.println("Big Mau");
+    }
+
+    public void voice() {
+        System.out.println("Mau");
+    }
 
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        peppy.voice();
+        sparky.voice2();
     }
 }
