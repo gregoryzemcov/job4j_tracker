8a388a583b10738032389ae807dcfb3f48b9eded
gregoryzemcov <griffondark@mail.ru>
2022-04-21 13:26:02 +0300

    1.2. Вызов метода объекта [#166085 #291369] Exercise 1

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -2,6 +2,10 @@ package ru.job4j.oop;
 
 public class Student {
 
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
     public void music(String lyrics) {
         System.out.println("I can sign a song : " + lyrics);
     }
@@ -9,6 +13,13 @@ public class Student {
     public static void main(String[] args) {
         Student petya = new Student();
         String song = "I believe, I can fly";
+        String songBeatles = "Let it bee";
+        String songSplin = "В каморке, что за актовым залом";
+        petya.music();
         petya.music(song);
+        petya.music();
+        petya.music(songBeatles);
+        petya.music();
+        petya.music(songSplin);
     }
 }
