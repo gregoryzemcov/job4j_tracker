73b4fc40ea7c40bae3e82870d58e83e5fcf3623c
gregoryzemcov <griffondark@mail.ru>
2022-06-21 11:45:47 +0300

    7. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,14 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
 
     private int id;
     private String name;
+
+    LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -31,5 +36,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
