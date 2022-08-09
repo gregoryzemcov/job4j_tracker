e79df6d758131c034923beb5ff0d2e80e0a13c23
gregoryzemcov <griffondark@mail.ru>
2022-08-09 09:48:22 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -10,35 +10,4 @@ import static org.hamcrest.core.IsNull.nullValue;
   */
 
 public class StartUITest {
-    @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    /** @ Test
-    public void whenEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
-        };
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName(), is("edited item"));
-    } */
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("item_1");
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId())};
-        assertThat(tracker.findById(item.getId()), is(nullValue()));
-    }
 }
