d688fc05b71c69af1a488445f9d71e3f52d160be
gregoryzemcov <griffondark@mail.ru>
2022-08-26 15:17:39 +0300

    Последнее.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
@@ -20,10 +25,11 @@ public class StartUI {
     }
 
    public static void main(String[] args) {
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        UserAction[] actions = {new CreateAction(), new EditAction(), new DeleteAction(),
+        UserAction[] actions = {new CreateAction(output), new EditAction(), new DeleteAction(),
                 new FindIdAction(), new FindAllAction(), new FindNameAction()};
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
     }
 }
