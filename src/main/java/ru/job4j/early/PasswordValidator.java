af4f284d7571a5b54881186ed5199f144683c4be
gregoryzemcov <griffondark@mail.ru>
2022-12-15 22:03:31 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #371097] Исправление

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,8 +1,12 @@
 package ru.job4j.early;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class PasswordValidator {
 
     public static String validate(String password) {
+        boolean upCase = false, loCase = false, digit = false, supp = false;
+        String[] strings = new String[]{"qwerty", "password", "admin", "user", "12345"};
 
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
@@ -12,23 +16,41 @@ public class PasswordValidator {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
 
-        if (password.toUpperCase().contains("qwerty".toUpperCase()) || password.contains("12345")
-                || password.toUpperCase().contains("password".toUpperCase())
-                || password.toUpperCase().contains("admin".toUpperCase())
-                || password.toUpperCase().contains("user".toUpperCase())) {
-            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+        for (String s : strings) {
+            if (password.toLowerCase().contains(s)) {
+                throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            }
         }
 
         for (char c : password.toCharArray()) {
-            if (!Character.isUpperCase(c)) {
-                throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-            } else if (!Character.isLowerCase(c)) {
-                throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-            } else if (!Character.isDigit(c)) {
-                throw new IllegalArgumentException("Password should contain at least one figure");
-            } else if (!Character.isSupplementaryCodePoint(c)) {
-                throw new IllegalArgumentException("Password should contain at least one special symbol");
+            if (Character.isUpperCase(c)) {
+                upCase = true;
             }
+            if (Character.isLowerCase(c)) {
+                loCase = true;
+            }
+            if (Character.isDigit(c)) {
+                digit = true;
+            }
+        }
+        Pattern p = Pattern.compile("[^A-Za-z0-9]");
+        Matcher m = p.matcher(password);
+        boolean b = m.find();
+        if (b) {
+            supp = true;
+        }
+
+        if (!upCase) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (!loCase) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (!digit) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (!supp) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
 
         return password;
