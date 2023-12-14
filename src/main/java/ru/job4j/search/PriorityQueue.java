1b2cc2edc9b78112a93b335e8e2b2b90d890e666
gregoryzemcov <griffondark@mail.ru>
2023-12-14 10:32:19 +0300

    5. Очередь с приоритетом на LinkedList [#41670 #465854] исправление

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -12,11 +12,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
-                index++;
-            } else if (element.getPriority() > task.getPriority()) {
+            if (element.getPriority() > task.getPriority()) {
                 break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
