9a53fe5517585d0c913ea8b67e907abaeafce4b4
gregoryzemcov <griffondark@mail.ru>
2023-12-20 15:05:24 +0300

    6. Конвертация ArrayList в двухмерный массив   [#10035 #467451] исправление

--- src/main/java/ru/job4j/collection/ConvertList2Array.java
+++ src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -10,9 +10,8 @@ public class ConvertList2Array {
         int cell = 0;
         for (Integer number : list) {
             array[row][cell] = number;
-            if (cell < cells - 1) {
-                cell++;
-            } else if (cell == cells - 1) {
+            cell++;
+            if (cell == cells) {
                 row++;
                 cell = 0;
             } else if (row == groups - 1) {
