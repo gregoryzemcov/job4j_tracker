8512524d7c72334466bbf58ecd6894fed70bf259
gregoryzemcov <griffondark@mail.ru>
2024-03-11 08:12:08 +0300

    1. Уникальные задачи. [#212653 #484997] исправление 1

--- src/main/java/ru/job4j/collection/FullSearch.java
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -1,15 +1,16 @@
 package ru.job4j.collection;
 
 import java.util.HashSet;
+import java.util.Set;
 import java.util.List;
 
 public class FullSearch {
 
-    public List<String> extractNumber(List<Task> tasks) {
+    public Set<String> extractNumber(List<Task> tasks) {
         HashSet<String> result = new HashSet<>();
         for (Task s : tasks) {
             result.add(String.valueOf(s.getNumber()));
         }
-        return List.copyOf(result);
+        return Set.copyOf(result);
     }
 }
