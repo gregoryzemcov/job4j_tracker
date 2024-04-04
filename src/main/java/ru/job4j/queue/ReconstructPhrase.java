ebd8219cddee65f5e7f62d88213eb4dfda2e853c
gregoryzemcov <griffondark@mail.ru>
2024-04-04 14:06:31 +0300

    2. Интерфейс Deque [#504943] 2

--- src/main/java/ru/job4j/queue/ReconstructPhrase.java
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -1,8 +1,6 @@
 package ru.job4j.queue;
 
 import java.util.Deque;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.lang.StringBuilder;
 
 public class ReconstructPhrase {
@@ -16,31 +14,22 @@ public class ReconstructPhrase {
 
     private String getEvenElements() {
         final int size = evenElements.size();
-        Deque<Character> stringEven = new LinkedList<>();
         StringBuilder strEven = new StringBuilder();
         for (int i = 0; i <= size - 1; i++) {
             Character ch = evenElements.poll();
             if (i % 2 == 0) {
-                stringEven.add(ch);
+                strEven.append(ch);
             }
-        }
-        for (Character s : stringEven) {
-            strEven.append(s);
         }
         return strEven.toString();
     }
 
     private String getDescendingElements() {
-        Deque<String> stringDescending = new LinkedList<>();
         StringBuilder strDescending = new StringBuilder();
-        Iterator<Character> iterator = descendingElements.descendingIterator();
-        while (iterator.hasNext()) {
-            stringDescending.add(String.valueOf(iterator.next()));
-        }
-        for (String s : stringDescending) {
+        for (Character s : descendingElements) {
             strDescending.append(s);
         }
-        return strDescending.toString();
+        return strDescending.reverse().toString();
     }
 
     public String getReconstructPhrase() {
