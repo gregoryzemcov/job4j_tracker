c58550c4ec370bbae79119abb23b1996ee13503c
gregoryzemcov <griffondark@mail.ru>
2025-01-22 10:03:58 +0300

    2. Комбинированный компаратор [#10036] job v.0.1

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -51,18 +51,18 @@ public class JobTest {
     public void whenComparatorByNameAndPriorityUp() {
         Comparator<Job> cmpNamePriority = new JobDescByNameUp().thenComparing(new JobDescByPriorityUp());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
+                new Job("Fix bug", 0),
                 new Job("Fix bug", 1)
         );
-        assertThat(rsl).isGreaterThan(0);
+        assertThat(rsl).isLessThan(0);
     }
 
     @Test
     public void whenComparatorByNameAndPriorityDown() {
         Comparator<Job> cmpNamePriority = new JobDescByNameDown().thenComparing(new JobDescByPriorityDown());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 1),
+                new Job("Impl task", 0)
         );
         assertThat(rsl).isGreaterThan(0);
     }
