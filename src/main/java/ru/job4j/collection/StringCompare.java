49195090f88bbb86b18e1a7e21f182430225d64b
GrigoriyZemtsov <griffondark@gmail.com>
2025-02-25 17:08:38 +0300

    3. Компаратор для строк [#35008] v.01

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -2,9 +2,18 @@ package ru.job4j.collection;
 
 import java.util.Comparator;
 
+/* Сравнение слов по символам */
+
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        return 0;
+        int result = 0;
+        for (int i = 0, j = 0; i < left.length(); i++) {
+            if (Integer.compare(left.charAt(i), right.charAt(j)) == 0) {
+                result = Character.compare(left.charAt(i), right.charAt(j));
+            }
+            j++;
+        }
+        return result;
     }
 }
