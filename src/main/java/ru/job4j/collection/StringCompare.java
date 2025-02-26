9f82533caf8e3b87647a45d33b62e22d0c1e6c41
gregoryzemcov <griffondark@mail.ru>
2025-02-26 08:57:43 +0300

    2. Комбинированный компаратор [#10036] job v.05

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -2,9 +2,17 @@ package ru.job4j.collection;
 
 import java.util.Comparator;
 
+/* Сравнение строк двух слов */
+
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        return 0;
+
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if ((left.charAt(i) != right.charAt(i))) {
+                return Character.compare(left.charAt(i), right.charAt(i));
+            }
+        }
+        return Integer.compare(left.length(), right.length());
     }
 }
